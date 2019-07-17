620d770fd8b89d91a2f2eab984d1aa02162508e5
Asmita Vadage <asmitaborawake@gmail.com>
2019-07-17 14:33:01 +0530

    code push

--- OnBoardingScreenDemo/Onboarding.swift
+++ OnBoardingScreenDemo/Onboarding.swift
@@ -8,23 +8,51 @@
 
 import UIKit
 
-class Onboarding: UIViewController {
+class Onboarding: UIView {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  static var instance = Onboarding()
+    
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    
+    func createSlide() -> [Onboarding]{
+        
+        let slide1:Onboarding = Bundle.main.loadNibNamed("Onboarding", owner: self, options: nil)?.first as! Onboarding
+        slide1.imgView.image = UIImage(named: "OB1")
+        slide1.imgView.alpha = 1.0
+        slide1.titleLbl.text = "Send money quickly"
+      //  slide1.titleLbl.textColor = .black
+        slide1.descLbl.text = "Your friends and family will receive your transfer within minutes, 24/7"
+       // slide1.descLbl.textColor = .black
+        slide1.imgView.contentMode = .scaleAspectFit
+        slide1.imgView.layer.masksToBounds = true
+        
+        let slide2:Onboarding = Bundle.main.loadNibNamed("Onboarding", owner: self, options: nil)?.first as! Onboarding
+        slide2.imgView.image = UIImage(named: "OB2")
+        slide2.imgView.alpha = 0.75
+        slide2.titleLbl.text = "Low cost"
+       // slide2.titleLbl.textColor = .black
+        slide2.descLbl.text = "We’ll always show you the costs upfront and never charge excessive fees"
+       // slide2.descLbl.textColor = .black
+        slide2.imgView.contentMode = .scaleAspectFit
+        slide2.imgView.layer.masksToBounds = true
+        
+        let slide3:Onboarding = Bundle.main.loadNibNamed("Onboarding", owner: self, options: nil)?.first as! Onboarding
+        slide3.imgView.image = UIImage(named: "OB3")
+        slide3.imgView.alpha = 0.75
+        slide3.titleLbl.text = "Trusted by millions"
+       // slide3.titleLbl.textColor = .black
+        slide3.descLbl.text = "22 million Mobile Money subscribers in 15 countries"
+       // slide3.descLbl.textColor = .black
+        slide3.imgView.contentMode = .scaleAspectFit
+        slide3.imgView.layer.masksToBounds = true
+        
+        return[slide1,slide2,slide3]
+        
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- OnBoardingScreenDemo/ViewController.swift
+++ OnBoardingScreenDemo/ViewController.swift
@@ -8,13 +8,113 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIScrollViewDelegate{
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageController: UIPageControl!
+    
+     var onboardingSlides:[Onboarding] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scrollView.delegate = self
+        self.createOnboardingSlides()
+       
+        
+    }
+    
+
+    func createOnboardingSlides(){
+        onboardingSlides = Onboarding.instance.createSlide()
+        setupSlideScrollView(slides: onboardingSlides)
+        
+        pageController.numberOfPages = onboardingSlides.count
+        pageController.currentPage = 0
+        view.bringSubviewToFront(pageController)
     }
 
-
+    
+    func setupSlideScrollView(slides : [Onboarding]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        print("frame height \(view.frame.height)")
+        print("frame width \(view.frame.width)")
+        print("frame width \(scrollView.frame.width)")
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(onboardingSlides.count), height: 1)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width , height: view.frame.height)
+            print("slides x is \(slides[i].frame.origin.x)")
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageController.currentPage = Int(pageIndex)
+        
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+        
+        
+        /*
+         * below code changes the background color of view on paging the scrollview
+         */
+        //        self.scrollView(scrollView, didScrollToPercentageOffset: percentageHorizontalOffset)
+        
+        
+        /*
+         * below code scales the imageview on paging the scrollview
+         */
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        print("offset if \(percentOffset.x)")
+        if(percentOffset.x > 0.30 && percentOffset.x <= 0.50) {
+            
+            UIView.animate(withDuration: 0.6,
+                           delay: 0,
+                           options: .curveEaseOut, //just an example, check UIViewAnimationOptions
+                animations: {
+                    self.onboardingSlides[0].imgView.alpha = 0.75
+                    self.onboardingSlides[1].imgView.alpha = 1.0
+            })
+            
+            
+        }  else if(percentOffset.x > 0.70 && percentOffset.x <= 1) {
+            //onboardingSlides[1].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
+            //onboardingSlides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
+            UIView.animate(withDuration: 0.6,
+                           delay: 0,
+                           options: .curveEaseOut, //just an example, check UIViewAnimationOptions
+                animations: {
+                    self.onboardingSlides[2].imgView.alpha = 1.0
+                    self.onboardingSlides[1].imgView.alpha = 0.75
+            })
+        } else if(percentOffset.x < 0.70 && percentOffset.x > 0.30){
+            UIView.animate(withDuration: 0.6,
+                           delay: 0,
+                           options: .curveEaseOut, //just an example, check UIViewAnimationOptions
+                animations: {
+                    self.onboardingSlides[1].imgView.alpha = 1.0
+                    self.onboardingSlides[2].imgView.alpha = 0.30
+            })
+        } else if(percentOffset.x < 0.30){
+            UIView.animate(withDuration: 0.6,
+                           delay: 0,
+                           options: .curveEaseOut, //just an example, check UIViewAnimationOptions
+                animations: {
+                    self.onboardingSlides[0].imgView.alpha = 1.0
+                    self.onboardingSlides[1].imgView.alpha = 0.75
+                    self.onboardingSlides[2].imgView.alpha = 0.75
+            })
+        }
+    }
+    
 }
 
